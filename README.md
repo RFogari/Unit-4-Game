e79f40c7a0b9e3c1c0b3f1f808bf8f5e762c7325
rfogari <fogari10@gmail.com>
2018-07-09 20:21:00 -0400

    Added link to live demo.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Unit-4-Game
 This project was to create an interactive game for web browsers using the JQuery library.  The goal of this game is for the player to match the randomly generated “lucky number” using the four dice images.  Each dice image is assigned a random value for that game.  When the player clicks an image, the value of that image will be added to the total player guess amount.  If the player clicks the right combination of images to match the lucky number, the player will win. 
+
+Link to live demo: https://rfogari.github.io/Unit-4-Game/
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,92 +19,78 @@ Win and loss does not reset.
 
 $(document).ready(function (){
 
-var win = 0;
-var loss = 0;
-var totalAmount = 0;
-
-
-//random number generated each time game resets
-var randomMatch = Math.floor((Math.random()*101+19))
-console.log(randomMatch);
-
-//add random number to HTML
-$(".randomNumber").html(randomMatch);
-
-//Push wins or losses to HTML
-$(".wins").html(win);
-$(".loses").html(loss);
-
-//object random numbers
-var object1 = Math.floor(Math.random()*11+1);
-var object2 = Math.floor(Math.random()*11+1);
-var object3 = Math.floor(Math.random()*11+1);
-var object4 = Math.floor(Math.random()*11+1);
-
-console.log(object1, object2, object3, object4);
-
-
-function won(){
-    win++;
-    $(".wins").text(win);
-    alert("You Win!!");
-    newGame();
-}
-
-function lost(){
-        loss++;
-        $(".loss").text(loss);
-        alert("You Lose!");
+    var win = 0;
+    var loss = 0;
+    var totalAmount = 0;
+    
+    
+    //random number generated each time game resets
+    var randomMatch = Math.floor((Math.random()*101+19))
+    console.log(randomMatch);
+    
+    //add random number to HTML
+    $(".randomNumber").html(randomMatch);
+    
+    //Push wins or losses to HTML
+    $(".wins").html(win);
+    $(".loses").html(loss);
+    
+    //object random numbers
+    var object1 = Math.floor(Math.random()*11+1);
+    var object2 = Math.floor(Math.random()*11+1);
+    var object3 = Math.floor(Math.random()*11+1);
+    var object4 = Math.floor(Math.random()*11+1);
+    
+    console.log(object1, object2, object3, object4);
+    
+    
+    function won(){
+        win++;
+        $(".wins").text(win);
+        alert("You Win!!");
         newGame();
-}
-
-
-
-
-//on click functions
-
-$(".object1").on("click", function (event) {
-    totalAmount = object1 + totalAmount;
-    $(".userTotal").text(totalAmount);
-    console.log(totalAmount);
-
-    if (totalAmount == randomMatch) {
-        won();
-    }    
-    else if  (totalAmount > randomMatch){
-        lost();
-    };
-});
-
-$(".object2").on("click", function (event) {
-    totalAmount = (totalAmount + object2);
-    $(".userTotal").text(totalAmount);
-    console.log(totalAmount);
-
-    if (totalAmount == randomMatch){
-        won();
-    }    
-    else if (totalAmount > randomMatch){
-        lost();
-    };
-});
-
-$(".object3").on("click", function (event) {
-    totalAmount = (totalAmount + object3);
-    $(".userTotal").text(totalAmount);
-    console.log(totalAmount);
-
-    if (totalAmount == randomMatch) {
-        won();
-    }    
-    else if (totalAmount > randomMatch){
-        lost();
-    };
+    }
+    
+    function lost(){
+            loss++;
+            $(".loss").text(loss);
+            alert("You Lose!");
+            newGame();
+    }
+    
+    
+    
+    
+    //on click functions
+    
+    $(".object1").on("click", function (event) {
+        totalAmount = object1 + totalAmount;
+        $(".userTotal").text(totalAmount);
+        console.log(totalAmount);
+    
+        if (totalAmount == randomMatch) {
+            won();
+        }    
+        else if  (totalAmount > randomMatch){
+            lost();
+        };
     });
     
+    $(".object2").on("click", function (event) {
+        totalAmount = (totalAmount + object2);
+        $(".userTotal").text(totalAmount);
+        console.log(totalAmount);
     
-    $(".object4").on("click", function (event) {
-        totalAmount = (totalAmount + object4);
+        if (totalAmount == randomMatch){
+            won();
+        }    
+        else if (totalAmount > randomMatch){
+            lost();
+        };
+    });
+    
+    $(".object3").on("click", function (event) {
+        totalAmount = (totalAmount + object3);
         $(".userTotal").text(totalAmount);
         console.log(totalAmount);
     
@@ -114,32 +100,46 @@ $(".object3").on("click", function (event) {
         else if (totalAmount > randomMatch){
             lost();
         };
+        });
+        
+        
+        $(".object4").on("click", function (event) {
+            totalAmount = (totalAmount + object4);
+            $(".userTotal").text(totalAmount);
+            console.log(totalAmount);
+        
+            if (totalAmount == randomMatch) {
+                won();
+            }    
+            else if (totalAmount > randomMatch){
+                lost();
+            };
+        });
+    
+        
+        //Win or Lose functions to alert player and update scores.
+       
+    
+    
+    
+    //restart game and generate new variable to match and new variable assigned to each object
+    
+    
+    
+    function newGame(){
+        var randomMatch = Math.floor((Math.random()*101+19));
+        console.log(randomMatch);
+        $(".randomNumber").text(randomMatch);
+        
+        object1 = Math.floor(Math.random()*11+1);
+        object2 = Math.floor(Math.random()*11+1);
+        object3 = Math.floor(Math.random()*11+1);
+        object4 = Math.floor(Math.random()*11+1);
+        console.log(object1, object2, object3, object4);
+    
+        totalAmount= 0;
+    
+        $(".userTotal").text(totalAmount);
+    };
+    
     });
-
-    
-    //Win or Lose functions to alert player and update scores.
-   
-
-
-
-//restart game and generate new variable to match and new variable assigned to each object
-
-
-
-function newGame(){
-    var randomMatch = Math.floor((Math.random()*101+19));
-    console.log(randomMatch);
-    $(".randomNumber").text(randomMatch);
-    
-    object1 = Math.floor(Math.random()*11+1);
-    object2 = Math.floor(Math.random()*11+1);
-    object3 = Math.floor(Math.random()*11+1);
-    object4 = Math.floor(Math.random()*11+1);
-    console.log(object1, object2, object3, object4);
-
-    totalAmount= 0;
-
-    $(".userTotal").text(totalAmount);
-};
-
-});
